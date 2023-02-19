6cb6f8a64329a2b9a5e769ddbc53ede560697757
Leonardo <leonardohenrique521@gmail.com>
2023-02-19 15:33:06 -0300

    Test database created

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long id;
+	
+	@Column
+	private String name;
+}
